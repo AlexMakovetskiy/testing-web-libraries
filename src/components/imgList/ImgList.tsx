9ab4698c54a90de9c0e4638058c72269d3efb3e5
AlexMakovetskiy <bo5634302@gmail.com>
2025-01-10 16:16:53 +0300

    feat: move code to separate repo

---
+++ src/components/imgList/ImgList.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+import ImagePreview from '../ImagePreview/ImagePreview';
+import { getImgList } from '../../api/getImgList';
+
+import './ImgList.css';
+
+interface IImgContentElement {
+    name: string;
+    img: string;
+}
+
+interface IImgListData {
+    images: IImgContentElement[];
+}
+
+function ImgList() {
+    const [imgListState, setImgListState] = useState<IImgListData>();
+
+    useEffect(() => {
+        (async function () {
+            const response: Response = await getImgList();
+            const imageContent: IImgListData = await response.json();
+            setImgListState(imageContent);
+        })();
+    }, []);
+
+    return (
+        <div className='imgListWrap'>
+            {
+                imgListState?.images.map((imgElement, index) => (
+                    <div key={index} className='imgElementWrap'>
+                        <p>{imgElement.name}</p>
+                        <ImagePreview base64String={imgElement.img}/>
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export default ImgList;
